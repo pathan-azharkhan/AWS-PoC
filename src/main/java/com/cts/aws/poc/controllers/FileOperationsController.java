a060661759871047528f8b4edc77cd5a91571a27
Azharkhan Pathan <pathan.azharkhan98@gmail.com>
2017-10-30 23:06:32 +0530

    Created a dummy web-page for file upload

--- src/main/java/com/cts/aws/poc/controllers/FileOperationsController.java
+++ src/main/java/com/cts/aws/poc/controllers/FileOperationsController.java
@@ -3,11 +3,12 @@
  */
 package com.cts.aws.poc.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.cts.aws.poc.services.FileStorageService;
@@ -16,13 +17,13 @@ import com.cts.aws.poc.services.FileStorageService;
  * @author Azharkhan
  *
  */
-@RestController
+@Controller
 public class FileOperationsController {
 	
 //	@Autowired
 	private FileStorageService fileStorageService;
 
-	@PostMapping("/file")
+	@PostMapping("/upload")
 	public String receivePaymentFile(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
 		
 //		fileStorageService.store(file);
@@ -31,4 +32,10 @@ public class FileOperationsController {
 		
 		return "redirect:/file";
 	}
+	
+	@GetMapping("/file")
+    public ModelAndView uploadScreen() {
+    	
+        return new ModelAndView("file");
+    }
 }
